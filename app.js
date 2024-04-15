2cef50217ba2df6876e4c4b662ff36de04890278
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-15 10:02:38 +0100

    Express, static, listen

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+// static assets
+app.use(express.static("./public"));
+// parse form data (middle)
+// Podemos por formularios para dentro da nossa app
+app.use(express.urlencoded({ extended: false })); // para o que se recebe do forms!!
+// Podemos por json para dentro da nossa app
+// parse json
+app.use(express.json());
+
+app.listen(5000, () => console.log("Im listening..."));
